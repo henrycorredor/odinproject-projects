0f870430cafbcbbe2eca035b5328356f15226759
Henry Corredor <henry@kurilabs.com>
2025-02-25 06:16:58 +0800

    stock picker complete

--- ruby/03_stockPicker.rb
+++ ruby/03_stockPicker.rb
@@ -3,27 +3,22 @@
 # @return [Array<integer, integer>] Index of the best day to buy / Index of the est day to sell
 def stock_picker(stock_prices)
   out = [0,0]
-  biggest = stock_prices[0]
-  smallest = stock_prices[0]
-
-  stock_prices.each do |price|
-    if price > biggest
-      biggest = price
+  buy_index = 0
+  stock_prices.reduce(0) do |biggest_profit,  buy_price|
+    sell_index = buy_index+1
+    stock_prices.last((stock_prices.length - 1) - buy_index).each do |sell_price|
+      profit = sell_price - buy_price
+      if profit > biggest_profit
+        biggest_profit = profit
+        out = [buy_index, sell_index]
+      end
+      sell_index += 1
     end
-    if price < smallest
-      smallest = price
-    end
+    buy_index+=1
+    biggest_profit
   end
-
   out
 end
-#   stock_prices.reduce([stock_prices[0], stock_prices[0]]) do |sum, price|
-#     actual_buy = sum[0]
-#     actual_sell = sum[1]
-#     if(prive <)
-#   end
-# end
 
-
-# > stock_picker([17,3,6,9,15,8,6,1,10])
+print stock_picker([17,3,6,9,15,8,6,1,10])
 # => [1,4]  # for a profit of $15 - $3 == $12
